a9dd0962e0deac121cbf39bab826cc8bd6d34966
HTA <hoangtuanh.180593@gmail.com>
2018-12-08 15:02:30 +0700

    add hotkey

--- src/ui/modal.js
+++ src/ui/modal.js
@@ -5,9 +5,23 @@ class Modal extends React.Component{
 	constructor(props){
 		super(props)
 		this._cancel = this._cancel.bind(this);
+		this._keyDown = this._keyDown.bind(this);
 	}
 	shouldComponentUpdate(nextProps, nextState){
 		return this.props.item !== nextProps.item;
+	}
+	componentDidMount(){
+		window.addEventListener('keydown',this._keyDown)
+	}
+	componentWillUnmount(){
+		window.removeEventListener('keydown',this._keyDown)
+	}
+	_keyDown(e){
+		if(e.key === "Escape"){
+			this._cancel();
+			e.stopPropagation();
+			e.preventDefault();
+		}
 	}
 	_cancel(data){
 		this.props.updateModalData(null)

4522d615ee5c1bf043ccca9af37ea4a09af5058b
HTA <hoangtuanh.180593@gmail.com>
2018-12-09 13:28:02 +0700

    update data

--- src/data/jdata.js
+++ src/data/jdata.js
@@ -2,7 +2,7 @@ const Data = require('data/data.json');
 
 //find current rank
 
-let date = Math.floor(Date.now()/ (1000*60*60*24));
+let date = 17874;//Math.floor(Date.now()/ (1000*60*60*24));
 var _data = Data;
 console.log(_data.length, date,  _data.slice(0,10));
 let def = _data.length + 10;

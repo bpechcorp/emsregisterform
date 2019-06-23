feaeab464a7bf14b0198b698a0cd76aef8b7855e
Hoang Tu Anh <hoangtuanh.180593@gmail.com>
2019-06-23 10:59:55 +0700

    fix exif

--- dist/bundle.js
+++ dist/bundle.js
@@ -38156,6 +38156,105 @@ var storageRef = firebase.storage().ref();
 var databaseRef = firebase.database().ref();
 databaseRef = databaseRef.child('newEvents');
 var storageRef = firebase.storage().ref();
+function getOrientation(file) {
+	return new Promise(function (resolve, reject) {
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			var view = new DataView(e.target.result);
+			if (view.getUint16(0, false) != 0xFFD8) {
+				return resolve(-2);
+			}
+			var length = view.byteLength,
+			    offset = 2;
+			while (offset < length) {
+				if (view.getUint16(offset + 2, false) <= 8) return resolve(-1);
+				var marker = view.getUint16(offset, false);
+				offset += 2;
+				if (marker == 0xFFE1) {
+					if (view.getUint32(offset += 2, false) != 0x45786966) {
+						return resolve(-1);
+					}
+
+					var little = view.getUint16(offset += 6, false) == 0x4949;
+					offset += view.getUint32(offset + 4, little);
+					var tags = view.getUint16(offset, little);
+					offset += 2;
+					for (var i = 0; i < tags; i++) {
+						if (view.getUint16(offset + i * 12, little) == 0x0112) {
+							return resolve(view.getUint16(offset + i * 12 + 8, little));
+						}
+					}
+				} else if ((marker & 0xFF00) != 0xFF00) {
+					break;
+				} else {
+					offset += view.getUint16(offset, false);
+				}
+			}
+			return resolve(-1);
+		};
+		reader.readAsArrayBuffer(file);
+	});
+}
+function getFileUrl(file) {
+	return new Promise(function (resolve, reject) {
+		var fr = new FileReader();
+		fr.onload = function () {
+			resolve(fr.result);
+		};
+		fr.readAsDataURL(file);
+	});
+}
+function resetOrientation(srcImg, srcOrientation) {
+	return new Promise(function (resolve, reject) {
+		var img = new Image();
+		img.onerror = reject;
+		img.onabort = reject;
+		img.onload = function () {
+			var width = img.width,
+			    height = img.height,
+			    canvas = document.createElement('canvas'),
+			    ctx = canvas.getContext("2d");
+
+			// set proper canvas dimensions before transform & export
+			if (4 < srcOrientation && srcOrientation < 9) {
+				canvas.width = height;
+				canvas.height = width;
+			} else {
+				canvas.width = width;
+				canvas.height = height;
+			}
+
+			// transform context before drawing image
+			switch (srcOrientation) {
+				case 2:
+					ctx.transform(-1, 0, 0, 1, width, 0);break;
+				case 3:
+					ctx.transform(-1, 0, 0, -1, width, height);break;
+				case 4:
+					ctx.transform(1, 0, 0, -1, 0, height);break;
+				case 5:
+					ctx.transform(0, 1, 1, 0, 0, 0);break;
+				case 6:
+					ctx.transform(0, 1, -1, 0, height, 0);break;
+				case 7:
+					ctx.transform(0, -1, -1, 0, height, width);break;
+				case 8:
+					ctx.transform(0, -1, 1, 0, 0, width);break;
+				default:
+					break;
+			}
+
+			// draw image
+			ctx.drawImage(img, 0, 0);
+			// export base64
+			canvas.toBlob(function (blob) {
+				if (!blob) return reject('invalid blob');
+				resolve(blob);
+			});
+		};
+		img.src = srcImg;
+	});
+}
 
 var Firebase = function () {
 	function Firebase() {
@@ -38163,8 +38262,35 @@ var Firebase = function () {
 	}
 
 	_createClass(Firebase, [{
+		key: "fixFile",
+		value: function fixFile(file) {
+			return new Promise(function (resolve, reject) {
+				getOrientation(file).then(function (orientation) {
+					console.error('orientation', orientation);
+					if (orientation <= 1) return resolve(file);
+					getFileUrl(file).then(function (url) {
+						resetOrientation(url, orientation).then(function (blob) {
+							console.error('resetOrientation new blob');
+							resolve(blob);
+						}).catch(reject);
+					}).catch(reject);
+				}).catch(reject);
+			});
+		}
+	}, {
 		key: "uploadFile",
 		value: function uploadFile(file) {
+			var _this = this;
+
+			return new Promise(function (resolve, reject) {
+				_this.fixFile(file).then(function (nfile) {
+					resolve(_this._uploadFile(nfile));
+				}).catch(reject);
+			});
+		}
+	}, {
+		key: "_uploadFile",
+		value: function _uploadFile(file) {
 			return new Promise(function (resolve, reject) {
 				var metaData = {};
 				var fname = Date.now() + '_' + file.name;
